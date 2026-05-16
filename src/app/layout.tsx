f4a1e13d3f29b284fe3b9fbfbbca322dc1e42565
dk <kudin.dima@gmail.com>
2026-05-16 19:54:25 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Nunito_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'PrintStudio | Customizable Personalization Platform',
+  description: 'Create and order custom products effortlessly. High-quality mugs, apparel, and more with our real-time design tool.',
+  openGraph: {
+    "title": "PrintStudio | Customizable Personalization Platform",
+    "description": "Create and order custom products effortlessly.",
+    "siteName": "PrintStudio"
+  },
+};
+
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${nunitoSans.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
